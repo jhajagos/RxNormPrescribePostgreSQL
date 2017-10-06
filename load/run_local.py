5cee5bec3f640cd53319b95a42c4b26655abb1e3
jhajagos <janos.hajagos@stonybrook.edu>
2017-10-06 13:19:50 -0400

    Added script

---
+++ load/run_local.py
@@ -0,0 +1,5 @@
+from generate_db_load_script import main
+
+main("scc_pps", "E:\\data\\rxnorm\\RxNorm_full_prescribe_07052016\\rrf\\", "postgres", "", "E:\\Program Files\\PostgreSQL\\9.4\\bin\\psql", rxnorm="rxnorm_prescribe")
+main("scc_pps", "E:\\data\\rxnorm\\RxNorm_full_07052016\\rrf\\", "postgres", "",
+         "E:\\Program Files\\PostgreSQL\\9.4\\bin\\psql")
